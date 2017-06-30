bc7eb5c00a1d1853049514ba78e45bb7f6d9f5b0
bjh970913 <bjh970913@gmail.com>
2017-06-30 17:48:05 +0900

    Add support for asar env

--- index.js
+++ index.js
@@ -334,8 +334,39 @@ function toCommandArgs(cmd, arch, keys) {
 
     return result
 }
+var fs = require('fs');
+var tempVsdDir = path.join(os.tmpdir(), 'node-regedit');
+
+copyVbsScriptToTemp = (function(){
+  var moved = false;
+  
+  var vbsLocation = path.join(__dirname, 'vbs');
+
+  return function () {
+    if (moved) {
+      return
+    }
+    if (!fs.existsSync(tempVsdDir)) {
+        fs.mkdirSync(tempVsdDir);
+    }
+
+    arr = fs.readdirSync(vbsLocation).filter(function(fileName){
+        return(ext = fileName.substr(-4)) === '.vbs' || ext === '.wsf'
+    })
+
+    console.log(arr, arr.length)
+
+    arr.map(function(c){
+        var content = fs.readFileSync(path.join(vbsLocation, c))
+        fs.writeFileSync(path.join(tempVsdDir, c), content);
+        console.log(path.join(tempVsdDir, c));
+    })
+  }
+})();
 
 //TODO: move to helper.js?
 function baseCommand(cmd, arch) {
-    return ['//Nologo', path.join(__dirname, 'vbs', cmd), arch]
+  copyVbsScriptToTemp();
+  return ['//Nologo', path.join(tempVsdDir, cmd), arch]
 }
+
